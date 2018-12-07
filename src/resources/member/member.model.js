dbc770ebec22812ca5fa6603ce8ca7db14c041db
Watcharapong <ice.pp@hotmail.com>
2018-12-07 09:44:11 +0700

    Add File Member controllers  , model , router

---
+++ src/resources/member/member.model.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+const memberSchema = new mongoose.Schema(
+  {
+    birthday: String,
+    blood_type: String,
+    english_first_name: String,
+    english_last_name: String,
+    facebook: String,
+    height: Number,
+    hobby: String,
+    instagram: String,
+    like: Array,
+    nickname: String,
+    province: String,
+    thai_first_name: String,
+    thai_last_name: String,
+    createdBy: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'member',
+      required: true
+    }
+  },
+  { timestamps: true }
+)
+
+export const Member = mongoose.model('member', memberSchema)
